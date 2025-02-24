b4556199b0f8aca84c50d5ef72d2ed9dceff829d
hd0rable <kanghuijin12@gmail.com>
2025-02-24 11:48:47 +0900

    [fix] 테스트 메스드명 관련 누락 수정

--- src/test/java/com/example/bookjourneybackend/domain/room/domain/repository/RoomRepositoryTest.java
+++ src/test/java/com/example/bookjourneybackend/domain/room/domain/repository/RoomRepositoryTest.java
@@ -23,6 +23,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static com.example.bookjourneybackend.domain.book.domain.GenreType.NOVEL_POETRY_DRAMA;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -112,7 +113,7 @@ class RoomRepositoryTest {
                 .imageUrl("profile.jpg")
                 .build());
 
-        UserRoom userRoom = new UserRoom(UserRole.MEMBER, 0.0, user, 1, room);
+        UserRoom userRoom = new UserRoom(UserRole.MEMBER, 0.0, user, 1, room, LocalDateTime.now());
         userRoomRepository.save(userRoom);
 
         entityManager.flush();
